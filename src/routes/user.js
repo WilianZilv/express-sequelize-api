dfa41b6c6f8e6eb7e175ebb56a6d498797732b13
Wilian <wilianzilv@gmail.com>
2020-05-28 20:19:59 -0300

    removed unused require

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,6 @@ const express = require("express");
 const route = express.Router();
 const userController = require("../controllers/user");
 const handle = require("../middlewares/globalHandler");
-const auth = require("../middlewares/auth");
 
 route.post("/", handle(userController.sign_in, true));
 route.put("/", handle(userController.sign_up, true));
