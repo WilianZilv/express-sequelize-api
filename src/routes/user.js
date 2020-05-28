f75b980d1f838bfc416862d299924e967ff4294f
Wilian <wilianzilv@gmail.com>
2020-05-28 20:18:31 -0300

    remove unused route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,6 @@ const userController = require("../controllers/user");
 const handle = require("../middlewares/globalHandler");
 const auth = require("../middlewares/auth");
 
-route.get("/plan", auth, handle(userController.current_plan));
 route.post("/", handle(userController.sign_in, true));
 route.put("/", handle(userController.sign_up, true));
 
